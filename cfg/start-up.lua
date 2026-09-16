f341694e4f7bc06f18d6e71018747b8460a8c23c
NKill <52850804+NKillHere@users.noreply.github.com>
2026-09-16 09:17:38 +0100

    Update start-up.lua

--- cfg/start-up.lua
+++ cfg/start-up.lua
@@ -1,5 +1,6 @@
 prequire("cfg/defaults")
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd(default.statusBar.. " && awww-daemon")
+    hl.exec_cmd(default.statusBar .. " && awww-daemon")
+    hl.exec_cmd("hyprpm reload")
 end)
